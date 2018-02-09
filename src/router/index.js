186374d41d040cc3606ab46fe67ac6d04047e202
768680109 <768680109@qq.com>
2018-02-09 16:11:08 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import Router from 'vue-router'
 const Index = resolve => require(['@/views/index'],resolve);
 
 const Lunbo = resolve => require(['@/components/lunbo'], resolve);
-const Dialog = resolve => require(['@/components/dialog'], resolve);
 const Choujiang = resolve => require(['@/components/choujiang/choujiang'], resolve);
 /*----------------------------------*/
 Vue.use(Router)
@@ -13,25 +12,8 @@ Vue.use(Router)
 export default new Router({
 		routes: [{
 			path: '/',
-			name: 'Index',
-			component: Index,
-			children: [
-			{
-				path: '/lunbo',
-				name: 'Lunbo',
-				component: Lunbo
-			},
-			{
-				path: '/dialog',
-				name: 'Dialog',
-				component: Dialog
-			},
-			{
-				path: '/choujiang',
-				name: 'Choujiang',
-				component: Choujiang
-			}
-			]
+			name: 'Choujiang',
+			component: Choujiang,
 		},
 		
 	]
